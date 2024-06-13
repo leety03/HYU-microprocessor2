ae0d4280e4750d6bac7f4caab0e93b69d2676446
Jisung Jang <omgthinker@naver.com>
2024-06-13 11:18:41 +0900

    Determine the slope scope for the balancing plate. Since we get non-constant value from IMU sensor, the tolerance range must be adjusted.

---
+++ activatebrake/SlopeManaging.ino
@@ -0,0 +1,25 @@
+#define UPPER_THRESHOLD 15 // Need Test on the real Slope 
+#define LOWER_THRESHOLD -15 // Need Test on the real Slope 
+#define Adjusment 2 // Need Test on the real Slope 
+static bool brakeFlag = false; 
+
+void brakingSystem(int16_t slopeValue) {
+    
+    if (slopeValue > UPPER_THRESHOLD + Adjusment) {
+        if (!brakeFlag) {
+            activateBrake_1();
+            activateBrake_2();
+            brakeFlag = true;
+        }
+    } else if (slopeValue < LOWER_THRESHOLD - Adjusment) {
+        if (!brakeFlag) {
+            activateBrake_1();
+            activateBrake_2();
+            brakeFlag = true; 
+        }
+    } else if (slopeValue < UPPER_THRESHOLD - Adjusment || slopeValue > LOWER_THRESHOLD + Adjusment) {
+        if (brakeFlag) {
+            return_brake();
+            brakeFlag = false; 
+    }
+}
